f3a6c339e424e53c33c4790c7976e4a7c24ef6d0
samrat-dotel <samratdotel7264@gmail.com>
2024-08-03 20:02:37 +0545

    initial commit

---
+++ src/components/MechanicForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MechanicForm = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MechanicForm
---
+++ src/components/MechanicList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MechanicList = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MechanicList
